284219669f4af6410fa33d2dcb5b8fd43167287d
Patrick <klibrdm@gmail.com>
2023-05-20 17:29:41 +0300

    Fixed storage issues

--- src/app/services/favorites.service.ts
+++ src/app/services/favorites.service.ts
@@ -15,28 +15,40 @@ export class FavoritesService {
 
   async init() {
     const favorites = await this.storageService.get("favorites");
-    this.favorites.next(favorites);
+    if (favorites) {
+      this.favorites.next(favorites);
+    }
   }
 
   async addFavorite(meal: Meal) {
     const favorites = await this.storageService.get("favorites");
-    favorites.push(meal);
-    this.storageService.set("favorites", favorites);
-    this.favorites.next(favorites);
+    if (favorites) {
+      favorites.push(meal);
+      this.storageService.set("favorites", favorites);
+      this.favorites.next(favorites);
+    } else {
+      this.storageService.set("favorites", [meal]);
+      this.favorites.next([meal]);
+    }
   }
 
   async removeFavorite(meal: Meal) {
     const favorites = await this.storageService.get("favorites");
-    const index = favorites.findIndex((f: Meal) => f.id === meal.id);
-    if (index !== -1) {
-      favorites.splice(index, 1);
-      this.storageService.set("favorites", favorites);
-      this.favorites.next(favorites);
+    if (favorites) {
+      const index = favorites.findIndex((f: Meal) => f.id === meal.id);
+      if (index !== -1) {
+        favorites.splice(index, 1);
+        this.storageService.set("favorites", favorites);
+        this.favorites.next(favorites);
+      }
     }
   }
 
   async isFavorite(mealId: string): Promise<boolean> {
     const favorites = await this.storageService.get("favorites");
-    return favorites.findIndex((f: Meal) => f.id === mealId) !== -1;
+    if (favorites) {
+      return favorites.findIndex((f: Meal) => f.id === mealId) !== -1;
+    }
+    return false;
   }
 }
